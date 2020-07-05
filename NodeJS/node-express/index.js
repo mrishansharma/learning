b5ef2f585ce7496fcda05ba8f4692aacf1f51f5c
mrishansharma <ishansharma501@gmail.com>
2020-07-05 19:26:07 +0530

    using morgan and express to create a javascript server node

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -1,17 +1,12 @@
 const express = require('express'), 
     http = require('http');
-
+const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-app.use((req,resp,next)=>{
-    console.log(req.headers);
-    resp.statusCode = 200;
-    resp.setHeader('Content-Type', 'text/html');
-    resp.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
-});
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
